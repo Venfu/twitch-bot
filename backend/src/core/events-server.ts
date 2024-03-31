9e0a95fb629183af05143506fa38aadec4fe39c3
Venfu <venfu@me.com>
2024-03-31 19:39:41 +0200

    Mise en place d'une file d'evenements

--- backend/src/core/events-server.ts
+++ backend/src/core/events-server.ts
@@ -4,7 +4,18 @@ import { EventToDisplay } from "../shared";
 let _eventsServer = {
   wss: new WebSocketServer({ port: 3001 }),
   ws: [] as WebSocket[],
+  events: [] as EventToDisplay[],
+  TIMEOUT: 5000,
   init(): Promise<void> {
+    setInterval(() => {
+      console.log("events", _eventsServer.events.length);
+      if (!_eventsServer.events.length || !_eventsServer.ws.length) return;
+      const event = _eventsServer.events.shift();
+      _eventsServer.ws.forEach((w: any) => {
+        w.send(JSON.stringify(event));
+      });
+    }, _eventsServer.TIMEOUT);
+
     return new Promise((res, rej) => {
       _eventsServer.wss.on("connection", (ws) => {
         ws.on("error", console.error);
@@ -13,13 +24,8 @@ let _eventsServer = {
       res();
     });
   },
-  pushEvent(event: EventToDisplay): Promise<void> {
-    return new Promise((res, rej) => {
-      _eventsServer.ws.forEach((w: any) => {
-        w.send(JSON.stringify(event));
-        res();
-      });
-    });
+  pushEvent(event: EventToDisplay): void {
+    this.events.push(event);
   },
 };
 
